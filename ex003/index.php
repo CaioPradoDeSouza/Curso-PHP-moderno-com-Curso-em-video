75f787ad660690f264705e0dba6c556a9a0b4f04
Caio Prado de Souza <cpssouza7@gmail.com>
2024-01-23 19:51:57 -0300

    Update

---
+++ ex003/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos PHP</title>
+</head>
+<body>
+    <h1>Teste de Tipos Primitivos</h1>
+    <?php 
+        // $num = 300;
+
+        // echo "O valor da váriavel é $num !";
+
+        // $n = 3e2;
+        // echo "$n";
+        // var_dump($n);
+
+        class Pessoa{
+            private string $nome;
+        }
+
+        $p = new Pessoa;
+        var_dump($p);
+    
+    ?>
+</body>
+</html>
